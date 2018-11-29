7796a2fff6b1bef559f117ea633a604a66607893
Colin Gross <grosscol@gmail.com>
2018-11-29 16:45:08 -0500

    Refactor reading spek to work with stdin as expected. Rework tests to check for correct function calls.   Sidestep trying to mock stdin.

--- R/read_spek.r
+++ R/read_spek.r
@@ -5,18 +5,17 @@
 #' @importFrom readr read_file
 read_spek <- function(spek_path = NULL){
   input <- resolve_spek_input(spek_path)
-  spek_str <- readr::read_file(input)
 
-  expanded <- jsonld::jsonld_expand(spek_str )
+  expanded <- jsonld::jsonld_expand(input)
   converted <- jsonlite::fromJSON(expanded, simplifyDataFrame = F)
-  return(converted[[1]])
+  return(converted)
 }
 
 resolve_spek_input <- function(spek_path){
   if(is.null(spek_path)){
-    res <- stdin()
+    res <- paste0( readLines('stdin') , collapse=" ")
   } else {
-    res <- spek_path
+    res <- readr::read_file(spek_path)
   }
   return(res)
 }
--- tests/testthat/test_resolve_spek_input.r
+++ tests/testthat/test_resolve_spek_input.r
@@ -1,9 +1,26 @@
+library(mockery)
 context("Resolve spek input")
 
-test_that("returns stdin given null", {
-  expect_identical( resolve_spek_input(NULL), stdin())
+DEMO_INPUT <- '{"foo":"bar"}'
+
+test_that("reads lines from stdin given no args", {
+  input_mock <- mock(DEMO_INPUT)
+  stub(resolve_spek_input,'readLines',input_mock)
+
+  # Check that the output is as expected
+  expect_equal( resolve_spek_input(NULL), DEMO_INPUT)
+
+  # Check that readLines was called with stdin
+  arg <- mock_args(input_mock)[[1]][[1]]
+  expect_identical(arg, 'stdin')
 })
 
-test_that("returns character string given string", {
-  expect_equal( resolve_spek_input("foo"), "foo")
+test_that("reads lines from filename arg", {
+  input_mock <- mock(DEMO_INPUT)
+  stub(resolve_spek_input,'readr::read_file',input_mock)
+
+  res <- resolve_spek_input('foo')
+
+  arg <- mock_args(input_mock)[[1]][[1]]
+  expect_identical(arg, 'foo')
 })
