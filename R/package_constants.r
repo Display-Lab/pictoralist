70b9387f79e23708474919c1be3d460c3704d096
Colin Gross <grosscol@gmail.com>
2019-01-02 12:33:41 -0500

    Correct package constants   Update ascribee uri   Add default package internal template location

--- R/package_constants.r
+++ R/package_constants.r
@@ -4,5 +4,7 @@ PT <- new.env()
 
 PT$CANDIDATE_URI <- "http://example.com/cpo#cpo_0000053"
 PT$ANC_TEMPLATE_URI <- "http://example.com/slowmo#AncestorTemplate"
-PT$ASCRIBEE_URI <- "http://example.com/slowmo#ascribee"
+PT$ASCRIBEE_URI <- "http://example.com/slowmo#AncestorPerformer"
 PT$PROMOTED_URI <- "http://example.com/slowmo#promoted_by"
+
+PT$DEFAULT_TEMPLATE_LOCATION <- system.file("templates", package="pictoralist")
