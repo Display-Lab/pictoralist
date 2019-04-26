1d76c40b45c03cb12fdf2277f67892afea3c0295
bshepp <bshepp@umich.edu>
2019-04-26 13:31:57 -0400

    Encapsulates plotting data to have better variable names (leaderboard)

--- inst/templates/Leaderboard.R
+++ inst/templates/Leaderboard.R
@@ -27,20 +27,51 @@ calculate_delta_shape <- function(delta) {
   ifelse(delta == 0, 23, ifelse(delta > 0, 24, 25))
 }
 
+# Creates list of green, red, gray arrows
 calculate_delta_color <- function(delta) {
   ifelse(delta==0, DL_GRAY, ifelse(delta > 0, DL_GREEN, DL_RED))
 }
 
+# Plots data
 make_plot <- function(plot_data) {
   plot_data %>% ggplot(aes(y=rank)) +
-    geom_label(x=1, aes(y=rank, label=rank)) +
-    geom_point(x=2, aes(y=rank, shape=delta_shape)) +
-    geom_label(x=3, aes(y=rank, label=delta_label)) +
-    geom_label(x=4, aes(y=rank, label=id)) +
-    geom_label(x=5, aes(y=rank, label=rate_label)) +
-    geom_label(x=6, aes(y=rank, label=count_label)) +
-    scale_shape_manual(values=c("23"=23,"24"=24,"25"=25)) +
-    scale_x_continuous(limits = c(1,7))
+    leaderboard_theme() +
+    geom_text(x=1, aes(y=max(rank)-rank, label=rank),
+              check_overlap=TRUE, color=DL_BLUE, hjust=0, size=3) +
+    geom_point(x=1.5, aes(y=max(rank)-rank, shape=delta_shape),
+               fill=delta_color, stroke=0, size=4) +
+    geom_text(x=2.1, aes(y=max(rank)-rank, label=delta_label),
+              check_overlap=TRUE, color=DL_BLUE, hjust=1, size=3, fontface="bold") +
+    geom_text(x=2.5, aes(y=max(rank)-rank, label=id),
+              check_overlap=TRUE, color=DL_BLUE, hjust=0, size=3) +
+    geom_text(x=7.5, aes(y=max(rank)-rank, label=rate_label, fontface="bold"),
+              check_overlap=TRUE, color=DL_BLUE, hjust=1, size=3) +
+    geom_text(x=9, aes(y=max(rank)-rank, label=count_label),
+              check_overlap=TRUE, color=DL_BLUE, hjust=1, size=3) +
+    geom_text(x=1, aes(y=max(rank), label="RANK"),
+              color=DL_BLUE, size=3, hjust=0) +
+    geom_text(x=2.5, aes(y=max(rank), label="CLINIC NAME"),
+              color=DL_BLUE, size=3, hjust=0) +
+    geom_text(x=7.5, aes(y=max(rank), label="COUNSEL RATE"),
+              color=DL_BLUE, size=3, hjust=1) +
+    geom_text(x=9, aes(y=max(rank), label="PATIENTS"),
+              color=DL_BLUE, size=3, hjust=1) +
+    scale_x_continuous(limits=c(1,9)) +
+    geom_hline(mapping=aes(yintercept=max(rank) - 0.3), color=DL_BLUE) +
+    geom_hline(mapping=aes(yintercept=-1), color=DL_BLUE) +
+    geom_text(x=1.5, aes(y=-1.3, label="UP RANK"),
+              color=DL_BLUE, size=3, hjust=0) +
+    geom_point(x=1, aes(y=-1.3),
+               shape=24, fill=DL_GREEN, stroke=0, size=4) +
+    geom_text(x=4.5, aes(y=-1.3, label="DOWN RANK"),
+              color=DL_BLUE, size=3, hjust=0) +
+    geom_point(x=4, aes(y=-1.3),
+               shape=25, fill=DL_RED, stroke=0, size=4) +
+    geom_text(x=7.5, aes(y=-1.3, label="NO CHANGE"),
+              color=DL_BLUE, size=3, hjust=0) +
+    geom_point(x=7, aes(y=-1.3),
+               shape=23, fill=DL_GRAY, stroke=0, size=4) +
+    scale_shape_identity()
 }
 
 # Removes grid and provides correct axis style
@@ -80,47 +111,4 @@ df$id = reorder(df$id, counsel_rate)
 # reorders data into descending order
 df <- df %>% arrange(100 - counsel_rate)
 
-
-foo <- ggplot(data=df, aes(y=rank)) +
-  leaderboard_theme() +
-  geom_text(x=1, aes(y=max(rank)-rank, label=rank),
-            check_overlap=TRUE, color=DL_BLUE, hjust=0, size=3) +
-  geom_point(x=1.5, aes(y=max(rank)-rank, shape=delta_shape),
-             fill=delta_color, stroke=0, size=4) +
-  geom_text(x=2.1, aes(y=max(rank)-rank, label=delta_label),
-            check_overlap=TRUE, color=DL_BLUE, hjust=1, size=3, fontface="bold") +
-  geom_text(x=2.5, aes(y=max(rank)-rank, label=id),
-            check_overlap=TRUE, color=DL_BLUE, hjust=0, size=3) +
-  geom_text(x=7.5, aes(y=max(rank)-rank, label=rate_label, fontface="bold"),
-            check_overlap=TRUE, color=DL_BLUE, hjust=1, size=3) +
-  geom_text(x=9, aes(y=max(rank)-rank, label=count_label),
-            check_overlap=TRUE, color=DL_BLUE, hjust=1, size=3) +
-  geom_text(x=1, aes(y=max(rank), label="RANK"),
-            color=DL_BLUE, size=3, hjust=0) +
-  geom_text(x=2.5, aes(y=max(rank), label="CLINIC NAME"),
-            color=DL_BLUE, size=3, hjust=0) +
-  geom_text(x=7.5, aes(y=max(rank), label="COUNSEL RATE"),
-            color=DL_BLUE, size=3, hjust=1) +
-  geom_text(x=9, aes(y=max(rank), label="PATIENTS"),
-            color=DL_BLUE, size=3, hjust=1) +
-  scale_x_continuous(limits=c(1,9)) +
-  geom_hline(mapping=aes(yintercept=max(rank) - 0.3), color=DL_BLUE) +
-  geom_hline(mapping=aes(yintercept=-1), color=DL_BLUE) +
-  geom_text(x=1.5, aes(y=-1.3, label="UP RANK"),
-            color=DL_BLUE, size=3, hjust=0) +
-  geom_point(x=1, aes(y=-1.3),
-             shape=24, fill=DL_GREEN, stroke=0, size=4) +
-  geom_text(x=4.5, aes(y=-1.3, label="DOWN RANK"),
-            color=DL_BLUE, size=3, hjust=0) +
-  geom_point(x=4, aes(y=-1.3),
-             shape=25, fill=DL_RED, stroke=0, size=4) +
-  geom_text(x=7.5, aes(y=-1.3, label="NO CHANGE"),
-            color=DL_BLUE, size=3, hjust=0) +
-  geom_point(x=7, aes(y=-1.3),
-             shape=23, fill=DL_GRAY, stroke=0, size=4) +
-  scale_shape_identity()
-
-foo
-#col_names <- c("RANK", "CLINIC NAME", "COUNSEL RATE", "PATIENTS")
 make_plot(df)
-
