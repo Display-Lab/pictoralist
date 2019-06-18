4b30cd446ddd1a7bef1760072eb3609087499deb
bshepp <bshepp@umich.edu>
2019-06-18 15:34:53 -0400

    Uses MTX data with denom as total_scripts and numer as high_dose_scripts

--- tests/testthat/test_integration_builtin_templates.r
+++ tests/testthat/test_integration_builtin_templates.r
@@ -1,6 +1,5 @@
 context("Integration test of baked in templates")
 
-
 test_that("Baked in templates work with mtx data",{
   mtx_data <- read_data(spekex::get_data_path("mtx"))
   mtx_spek <- spekex::read_spek(spekex::get_spek_path("mtx"))
@@ -11,4 +10,25 @@ test_that("Baked in templates work with mtx data",{
                     recip = "E87746", data=mtx_data, spek=mtx_spek)
   is_ggplot <- sapply(results, function(x){"ggplot" %in% class(x)})
   expect_true(all(is_ggplot))
+})
+
+test_that("Data provided is used in baked in Top Performer Template", {
+  mtx_data <- read_data(spekex::get_data_path("mtx"))
+  mtx_spek <- spekex::read_spek(spekex::get_spek_path("mtx"))
+
+  templates <- load_templates()
+
+  denom_colname <- 'total_scripts'
+  numer_colname <- 'high_dose_scripts'
+
+  recip_data <- filter(mtx_data, mtx_data$practice == "E87746")
+  data_denom <- sum(recip_data[denom_colname])
+  data_numer <- sum(recip_data[numer_colname])
+
+  results <- lapply(templates, FUN=function(t, recip, data, spek){t$run(recip, data, spek)},
+                    recip = "E87746", data=mtx_data, spek=mtx_spek)
+  template_denom <- results$TopPerformerGraph$data$value[1]
+  template_recip <- results$TopPerformerGraph$data$id[1]
+  expect_true(template_denom == data_denom)
+  expect_true(template_recip == "E87746")
 })
