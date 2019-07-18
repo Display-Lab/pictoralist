71160513a9e64078bd7b94d521a94b73291a897d
Colin Gross <grosscol@gmail.com>
2019-07-18 15:31:54 -0400

    Get produce plots test to run   Provided expected dummy promoted candidates   Preserve names of templates env list to allow lookup by name to work.

--- tests/testthat/test_produce_plots.R
+++ tests/testthat/test_produce_plots.R
@@ -3,25 +3,27 @@ library(tibble)
 
 context('Produce plots')
 
-# Dummy templates
-foo <- new.env()
-foo$run <- function(data, spek){ ggplot2::ggplot() }
-bar <- new.env()
-bar$run <- function(data, spek){ ggplot2::ggplot() }
-dummy_templates <- list('foo'=foo, 'bar'=bar)
-
-va_data <- read_data(spekex::get_data_path("va"))
-va_spek <- spekex::read_spek(spekex::get_spek_path("va"))
-
 templates <- load_templates()
 
+CANDI_1 <- list( `@id` = "http://example.com/app/candidate1",
+                 `@type` = "http://example.com/cpo#cpo_0000053")
+CANDI_1[PT$ANC_PERFORMER_URI] <- list(list(`@value` = paste0(PT$APP_BASE_URI,"E87746")))
+CANDI_1[PT$ANC_TEMPLATE_URI]  <- list( list(`@value` = paste0(PT$APP_BASE_URI,"TopPerformerGraph")))
+CANDI_1[PT$PROMOTED_URI] <- list(list(`@id` = "http://example.com/slowmo#default_esteemer_criteria"))
 
+CANDI_2 <- list( `@id` = "http://example.com/app/candidate2",
+                 `@type` = "http://example.com/cpo#cpo_0000053")
+CANDI_2[PT$ANC_PERFORMER_URI] <- list(list(`@value` = paste0(PT$APP_BASE_URI,"E87746")))
+CANDI_2[PT$ANC_TEMPLATE_URI]  <- list( list(`@value` = paste0(PT$APP_BASE_URI,"IUDGraph")))
+CANDI_2[PT$PROMOTED_URI] <- list(list(`@id` = "http://example.com/slowmo#default_esteemer_criteria"))
+
+PROMOTED_CANDIDATES <- list(CANDI_1, CANDI_2 )
 
 test_that("retuns a list of ggplot objects with mtxdata",{
   mtx_data <- read_data(spekex::get_data_path("mtx"))
   mtx_spek <- spekex::read_spek(spekex::get_spek_path("mtx"))
-  promoted <- "E87746"
-  mtx_templates <- c(templates$TopPerformerGraph, templates$IUDGraph)
+  template_idxs <- match(c("TopPerformerGraph", "IUDGraph"), names(templates))
+  mtx_templates <- templates[template_idxs]
 
-  result <- produce_plots(promoted, mtx_templates, mtx_data, mtx_spek)
+  result <- produce_plots(PROMOTED_CANDIDATES, mtx_templates, mtx_data, mtx_spek)
 })
